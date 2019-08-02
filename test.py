cca4802b2bfe1c83e5bab3055d09a887982589a9
Shahriyar Shawon <shahriyarshawon@Shahriyars-MacBook-Pro.local>
2019-08-01 20:26:56 -0400

    creates gif from flags-ultra

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os 
 
-for root, directories, files in os.walk("flags-mini"):
+for root, directories, files in os.walk("flags-ultra"):
     for file in files:
         # print(file)
         os.system(f"python wave_flag.py {root}/{file}")
