625350be474afca21e0dff6cf089693a14bd0388
zoox101 <william@thebookers.net>
2019-09-02 21:45:03 -0500

    Modified Requirements.txt

--- tests/class_tester.py
+++ tests/class_tester.py
@@ -1,5 +1,6 @@
-# from ..FlagWaver.FlagWaver import FlagWaver
-from FlagWaver.FlagWaver import FlagWaver
+#from ..FlagWaver.FlagWaver import FlagWaver
+#from FlagWaver.FlagWaver import FlagWaver
+#from FlagWaver.FlagWaver import FlagWaver
 
 # GRADIENT = "flag-wave/tests/source/color_flag_mask.gif"
 # WHITE = "flag-wave/tests/source/white_flag.gif"
@@ -9,5 +10,5 @@ GRADIENT = "source/color_flag_mask.gif"
 WHITE = "source/white_flag.gif"
 SHADOW = "source/white_flag_shadow.gif"
 
-flag_waver = FlagWaver(GRADIENT,WHITE,SHADOW)
-flag_waver.create_gif("nm.gif")
+flag_waver = FlagWaver(GRADIENT, WHITE, SHADOW)
+flag_waver.create_gif('France.png')
